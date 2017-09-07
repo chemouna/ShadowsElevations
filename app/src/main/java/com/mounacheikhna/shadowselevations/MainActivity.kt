ea1423961ba675988e5d78bef10198dc6ebc63df
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-09-07 23:59:17 +0100

    Update.

--- app/src/main/java/com/mounacheikhna/shadowselevations/MainActivity.kt
+++ app/src/main/java/com/mounacheikhna/shadowselevations/MainActivity.kt
@@ -2,11 +2,15 @@ package com.mounacheikhna.shadowselevations
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.AppCompatButton
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
 }
