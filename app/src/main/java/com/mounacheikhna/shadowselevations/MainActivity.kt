ccdcaaf98db8540c885aad1248c2299e273d69b9
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-09-12 18:21:06 +0100

    Playing with edit text and its background.

--- app/src/main/java/com/mounacheikhna/shadowselevations/MainActivity.kt
+++ app/src/main/java/com/mounacheikhna/shadowselevations/MainActivity.kt
@@ -2,8 +2,6 @@ package com.mounacheikhna.shadowselevations
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.AppCompatButton
-import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
